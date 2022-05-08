20119770c3a9e6d1aa758bf60629380c07b45560
atakriti <anwart256@gmail.com>
2022-05-08 14:49:32 +0200

    pop ix is done with js

---
+++ src/javascript/main.js
@@ -0,0 +1,9 @@
+pop.style.display = "block";
+let iX = () => {
+    let pop = document.getElementById("pop");
+    if (pop.style.display === "block") {
+        pop.style.display = "none"
+    } else {
+        pop.style.display = "block"
+    }
+}
